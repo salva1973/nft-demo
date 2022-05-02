10cc7e9d7ed634ca002ef780a4bbd5c5979d9863
Salvatore Vivolo <3092390+salva1973@users.noreply.github.com>
2022-05-02 08:31:55 +0200

    continue NFT dogie

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -3,3 +3,19 @@
 pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollectible is ERC721 { // NFT contract known as Factory contract
+
+  uint256 public tokenCounter;
+
+  constructor () public ERC721("Dogie", "DOG") { // name and symbol
+    tokenCounter = 0;
+  }
+
+  function createCollectible() public returns (uint256) {
+    uint256 newTokenId = tokenCounter;
+    _safeMint(msg.sender, newTokenId);
+    tokenCounter = tokenCounter + 1;
+    return newTokenId;
+  }
+}
