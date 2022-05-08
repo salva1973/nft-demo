bf1676500ff23d4144ab8b77666b392376bc505e
Salvatore Vivolo <3092390+salva1973@users.noreply.github.com>
2022-05-08 10:29:16 +0200

    added nft uri

--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -1,7 +1,13 @@
-from scripts.helpful_scripts import get_account, simpleCollectible
+from scripts.helpful_scripts import get_account
+from brownie import SimpleCollectible
 
 sample_token_uri = "ipfs://Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
 
 def main():
   account = get_account()
-  simple_collectible = simpleCollectible.deploy({"from": account})
+  simple_collectible = SimpleCollectible.deploy({"from": account})
+  tx = simple_collectible.createCollectible(sample_token_uri, {"from": account})
+  tx.wait(1)
+  print(f"Awesome, you can view your NFT at {OPENSEA_URL.format(simple_collectible.address, simple_collectible.tokenCounter() - 1)}")
+  print("Please wait up to 20 minutes, and hit the refresh metadata button.")
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -16,7 +16,8 @@ def get_account(index=None, id=None):
     return accounts[0]
   if id:
     return accounts.load(id)
-  if network.show_active() in config["networks"]:
-    return accounts.add(config["wallets"]["from_key"])
-  return None
+  return accounts.add(config["wallets"]["from_key"])
+  # if network.show_active() in config["networks"]:
+  #   return accounts.add(config["wallets"]["from_key"])
+  # return None
   
